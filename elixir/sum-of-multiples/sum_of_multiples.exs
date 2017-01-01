77848976c4ed8ff4af8be01253a8f04ccd56528f
Andrew <andrew@huth.me>
2016-12-31 18:20:37 -0800

    Elixir: sum-of-multiples

---
+++ elixir/sum-of-multiples/sum_of_multiples.exs
@@ -0,0 +1,15 @@
+defmodule SumOfMultiples do
+  @doc """
+  Adds up all numbers from 1 to a given end number that are multiples of the factors provided.
+  """
+  @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
+  def to(limit, factors) do
+    1..(limit - 1)
+    |> Enum.filter(fn(number) ->
+      Enum.any?(factors, fn(factor) ->
+        rem(number, factor) == 0
+      end)
+    end)
+    |> Enum.sum
+  end
+end

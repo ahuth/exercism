8c1f753f5b98fcf93dd132265304e394910a63cc
Andrew <andrew@huth.me>
2017-01-01 22:24:32 -0800

    Elixir: update roman-numerals

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -7,55 +7,21 @@ defmodule Roman do
     deduce(number, 1000, "")
   end
 
-  defp deduce(number, 1000, acc) do
-    deduce(rem(number, 1000), 900, acc <> String.duplicate("M", div(number, 1000)))
-  end
+  defp deduce(number, 1000, acc), do: process(number, acc, 1000, 900, "M")
+  defp deduce(number, 900, acc), do: process(number, acc, 900, 500, "CM")
+  defp deduce(number, 500, acc), do: process(number, acc, 500, 400, "D")
+  defp deduce(number, 400, acc), do: process(number, acc, 400, 100, "CD")
+  defp deduce(number, 100, acc), do: process(number, acc, 100, 90, "C")
+  defp deduce(number, 90, acc), do: process(number, acc, 90, 50, "XC")
+  defp deduce(number, 50, acc), do: process(number, acc, 50, 40, "L")
+  defp deduce(number, 40, acc), do: process(number, acc, 40, 10, "XL")
+  defp deduce(number, 10, acc), do: process(number, acc, 10, 9, "X")
+  defp deduce(number, 9, acc), do: process(number, acc, 9, 5, "IX")
+  defp deduce(number, 5, acc), do: process(number, acc, 5, 4, "V")
+  defp deduce(number, 4, acc), do: process(number, acc, 4, 1, "IV")
+  defp deduce(number, 1, acc), do: acc <> String.duplicate("I", number)
 
-  defp deduce(number, 900, acc) do
-    deduce(rem(number, 900), 500, acc <> String.duplicate("CM", div(number, 900)))
-  end
-
-  defp deduce(number, 500, acc) do
-    deduce(rem(number, 500), 400, acc <> String.duplicate("D", div(number, 500)))
-  end
-
-  defp deduce(number, 400, acc) do
-    deduce(rem(number, 400), 100, acc <> String.duplicate("CD", div(number, 400)))
-  end
-
-  defp deduce(number, 100, acc) do
-    deduce(rem(number, 100), 90, acc <> String.duplicate("C", div(number, 100)))
-  end
-
-  defp deduce(number, 90, acc) do
-    deduce(rem(number, 90), 50, acc <> String.duplicate("XC", div(number, 90)))
-  end
-
-  defp deduce(number, 50, acc) do
-    deduce(rem(number, 50), 40, acc <> String.duplicate("L", div(number, 50)))
-  end
-
-  defp deduce(number, 40, acc) do
-    deduce(rem(number, 40), 10, acc <> String.duplicate("XL", div(number, 40)))
-  end
-
-  defp deduce(number, 10, acc) do
-    deduce(rem(number, 10), 9, acc <> String.duplicate("X", div(number, 10)))
-  end
-
-  defp deduce(number, 9, acc) do
-    deduce(rem(number, 9), 5, acc <> String.duplicate("IX", div(number, 9)))
-  end
-
-  defp deduce(number, 5, acc) do
-    deduce(rem(number, 5), 4, acc <> String.duplicate("V", div(number, 5)))
-  end
-
-  defp deduce(number, 4, acc) do
-    deduce(rem(number, 4), 1, acc <> String.duplicate("IV", div(number, 4)))
-  end
-
-  defp deduce(number, 1, acc) do
-    acc <> String.duplicate("I", number)
+  defp process(number, acc, current, next, symbol) do
+    deduce(rem(number, current), next, acc <> String.duplicate(symbol, div(number, current)))
   end
 end

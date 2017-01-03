b81f41aa2a258e7ebb3b9c82e095b1b35d3bf1a7
Andrew <andrew@huth.me>
2017-01-02 20:41:36 -0800

    Javascript: update word-count

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,10 +1,10 @@
 const punctuation = new RegExp("[!¡¿?&@$%^&:.]", "g")
 const separators = /[,\s]+/
+const quoted = /'(\w+)'/
 const WordCount = function () {}
 
 WordCount.prototype.count = (sentence) => {
-  return sanitize(sentence).split(separators).reduce((counts, word) => {
-    word = word.replace(/^'|'$/g, "")
+  return separate(sentence).reduce((counts, word) => {
     counts = Object.assign({[word]: 0}, counts)
     counts[word] = counts[word] + 1
     return counts
@@ -12,7 +12,15 @@ WordCount.prototype.count = (sentence) => {
 }
 
 const sanitize = (sentence) => {
-  return sentence.trim().toLowerCase().replace(punctuation, "")
+  return unquote(sentence.trim().toLowerCase().replace(punctuation, ""))
+}
+
+const separate = (sentence) => {
+  return sanitize(sentence).split(separators)
+}
+
+const unquote = (sentence) => {
+  return sentence.replace(quoted, "$1")
 }
 
 module.exports = WordCount

2e29d11fbefbc26c64f8a6310c341385f3dee68c
Andrew <andrew@huth.me>
2017-03-17 23:37:01 -0700

    Elixir: rotational-cipher

---
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -0,0 +1,41 @@
+defmodule RotationalCipher do
+  @doc """
+  Given a plaintext and amount to shift by, return a rotated string.
+
+  Example:
+  iex> RotationalCipher.rotate("Attack at dawn", 13)
+  "Nggnpx ng qnja"
+  """
+  @spec rotate(text :: String.t(), shift :: integer) :: String.t()
+  def rotate(text, 0), do: text
+  def rotate(text, 26), do: text
+  def rotate(text, shift) do
+    text
+    |> String.codepoints
+    |> shift_each(shift)
+    |> Enum.join
+  end
+
+  defp shift_each(codepoints, shift) do
+    Enum.map(codepoints, &(shift_one(&1, shift)))
+  end
+
+  defp shift_one(codepoint, shift) do
+    case String.match?(codepoint, ~r/[A-Za-z]/) do
+      true ->
+        <<b>> = codepoint
+        <<encode(b, shift)>>
+      false ->
+        codepoint
+    end
+  end
+
+  defp encode(ascii, shift) when ascii < 91, do: encode_upper(ascii, shift)
+  defp encode(ascii, shift), do: encode_lower(ascii, shift)
+
+  defp encode_upper(ascii, shift) when ascii + shift > 90, do: ascii + shift - 26
+  defp encode_upper(ascii, shift), do: ascii + shift
+
+  defp encode_lower(ascii, shift) when ascii + shift > 122, do: ascii + shift - 26
+  defp encode_lower(ascii, shift), do: ascii + shift
+end

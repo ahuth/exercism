dce788dc294d3401b31af2f19ff7d80a93a00ed4
Andrew <andrew@huth.me>
2017-03-25 14:17:19 -0700

    Clojure: clock update

--- clojure/clock/src/clock.clj
+++ clojure/clock/src/clock.clj
@@ -1,26 +1,15 @@
 (ns clock)
 
-(defn- zero-pad [num]
-  (if (< num 10)
-    (str "0" num)
-    (str num)))
-
-(defn- wrap-around [num to]
-  (cond
-    (< num 0) (+ to (rem num to))
-    (>= num to) (rem num to)
-    :else num))
-
 (defn clock [hour min]
   (->
     hour
     (* 60)
     (+ min)
-    (wrap-around 1440)))
+    (mod 1440)))
 
 (defn clock->string [min]
   (let [hours (quot min 60) remaining-min (rem min 60)]
-    (str (zero-pad hours) ":" (zero-pad remaining-min))))
+    (format "%02d:%02d" hours remaining-min)))
 
 (defn add-time [min added]
   (clock 0 (+ min added)))

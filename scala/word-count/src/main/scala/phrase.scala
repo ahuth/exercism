6789ca0aba74d949e61e08f63a757d69742c9b7f
Andrew <ahuth@mavenlink.com>
2017-04-08 18:38:54 -0700

    Scala: word-count update

--- scala/word-count/src/main/scala/phrase.scala
+++ scala/word-count/src/main/scala/phrase.scala
@@ -1,3 +1,3 @@
 class Phrase(phrase: String) {
-  val wordCount = phrase.toLowerCase().split("[^a-z0-9']+").groupBy(identity).mapValues(_.size)
+  val wordCount = phrase.toLowerCase().split("[^\\w']+").groupBy(identity).mapValues(_.size)
 }

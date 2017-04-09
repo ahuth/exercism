9459cccb7aedd9f668a15d8057c31118e519f433
Andrew <ahuth@mavenlink.com>
2017-04-09 15:28:43 -0700

    Scala: anagram update

--- scala/anagram/src/main/scala/anagram.scala
+++ scala/anagram/src/main/scala/anagram.scala
@@ -1,20 +1,7 @@
 class Anagram(word: String) {
-  private[this] val normalized = word.toLowerCase.sorted
-  private[this] val process = removeWord _ andThen selectAnagrams _
+  private[this] val lowered = word.toLowerCase
 
   def matches(candidates: Seq[String]): Seq[String] = {
-    process(candidates)
-  }
-
-  private[this] def removeWord(list: Seq[String]): Seq[String] = {
-    list.filterNot(x => x.toLowerCase == word.toLowerCase)
-  }
-
-  private[this] def selectAnagrams(list: Seq[String]): Seq[String] = {
-    list.filter(x => normalize(x) == normalized)
-  }
-
-  private[this] def normalize(word: String): String = {
-    word.toLowerCase.sorted
+    candidates.filterNot(_.toLowerCase == lowered).filter(_.toLowerCase.sorted == lowered.sorted)
   }
 }

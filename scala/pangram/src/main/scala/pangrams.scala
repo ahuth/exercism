41dddba14e2765dad2061f2bb50ed4b9fc736cd2
Andrew <ahuth@mavenlink.com>
2017-04-09 07:44:03 -0700

    Scala: pangram update

--- scala/pangram/src/main/scala/pangrams.scala
+++ scala/pangram/src/main/scala/pangrams.scala
@@ -1,5 +1,5 @@
 object Pangrams {
   def isPangram(phrase: String): Boolean = {
-    phrase.toLowerCase().replaceAll("[^a-z]", "").distinct.length == 26
+    phrase.toLowerCase.filter(_.isLetter).distinct.length == 26
   }
 }

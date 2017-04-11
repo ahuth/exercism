cf09b9b088562a5588528791b0191b8cb67384d2
Andrew <ahuth@mavenlink.com>
2017-04-10 21:21:13 -0700

    Clojure: space-age update

--- clojure/space-age/src/space_age.clj
+++ clojure/space-age/src/space_age.clj
@@ -3,23 +3,13 @@
 (defn on-earth [seconds]
   (/ seconds 31557600))
 
-(defn on-mercury [seconds]
-  (/ (on-earth seconds) 0.2408467))
+(defmacro defplanet [name period]
+  `(defn ~(symbol (str "on-" name)) [~'seconds] (/ (on-earth ~'seconds) ~period)))
 
-(defn on-venus [seconds]
-  (/ (on-earth seconds) 0.61519726))
-
-(defn on-mars [seconds]
-  (/ (on-earth seconds) 1.8808158))
-
-(defn on-jupiter [seconds]
-  (/ (on-earth seconds) 11.862615))
-
-(defn on-saturn [seconds]
-  (/ (on-earth seconds) 29.4474985))
-
-(defn on-uranus [seconds]
-  (/ (on-earth seconds) 84.016846))
-
-(defn on-neptune [seconds]
-  (/ (on-earth seconds) 164.79132))
+(defplanet mercury 0.2408467)
+(defplanet venus 0.61519726)
+(defplanet mars 1.8808158)
+(defplanet jupiter 11.862615)
+(defplanet saturn 29.4474985)
+(defplanet uranus 84.016846)
+(defplanet neptune 164.79132)

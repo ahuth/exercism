a7264300393b2b1500c09c869f70b0ce5c803191
Andrew <ahuth@mavenlink.com>
2017-04-12 21:45:54 -0700

    Scala: robot-name update

--- scala/robot-name/src/main/scala/Robot.scala
+++ scala/robot-name/src/main/scala/Robot.scala
@@ -1,14 +1,10 @@
+import scala.util.Random.{alphanumeric => an}
+
 class Robot {
   var name = generateName
+  def reset() = name = generateName
 
-  def reset() = {
-    name = generateName
-  }
-
-  private[this] def generateName = {
-    val stream = scala.util.Random.alphanumeric
-    val alphas = stream.filter(_.isLetter).take(2).mkString.toUpperCase
-    val numbers = stream.filter(_.isDigit).take(3).mkString.toUpperCase
-    s"$alphas$numbers"
+  private def generateName = {
+    (an.filter(_.isUpper).take(2) ++ an.filter(_.isDigit).take(3)).mkString
   }
 }

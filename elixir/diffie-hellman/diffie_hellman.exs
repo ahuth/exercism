4c6a7de6313ce629233e92753d167b55ebc20396
Andrew <ahuth@mavenlink.com>
2017-04-15 09:32:05 -0700

    Elixir: diffie-hellman

---
+++ elixir/diffie-hellman/diffie_hellman.exs
@@ -0,0 +1,36 @@
+defmodule DiffieHellman do
+  @doc """
+  Given a prime integer `prime_p`, return a random integer between 1 and `prime_p` - 1
+  """
+  @spec generate_private_key(prime_p :: integer) :: integer
+  def generate_private_key(prime_p) do
+    :rand.uniform(prime_p - 1)
+  end
+
+  @doc """
+  Given two prime integers as generators (`prime_p` and `prime_g`), and a private key,
+  generate a public key using the mathematical formula:
+
+  (prime_g **  private_key) % prime_p
+  """
+  @spec generate_public_key(prime_p :: integer, prime_g :: integer, private_key :: integer) :: integer
+  def generate_public_key(prime_p, prime_g, private_key) do
+    mod_pow(prime_g, private_key, prime_p)
+  end
+
+  @doc """
+  Given a prime integer `prime_p`, user B's public key, and user A's private key,
+  generate a shared secret using the mathematical formula:
+
+  (public_key_b ** private_key_a) % prime_p
+  """
+  @spec generate_shared_secret(prime_p :: integer, public_key_b :: integer, private_key_a :: integer) :: integer
+  def generate_shared_secret(prime_p, public_key_b, private_key_a) do
+    mod_pow(public_key_b, private_key_a, prime_p)
+  end
+
+  defp mod_pow(n, p, m) do
+    :crypto.mod_pow(n, p, m)
+    |> :binary.decode_unsigned
+  end
+end

04e85537b5f0a08d00ee429d452b56b6fb85f404
Andrew <ahuth@mavenlink.com>
2017-04-21 23:41:48 -0700

    Elixir: tournament update

--- elixir/tournament/tournament.exs
+++ elixir/tournament/tournament.exs
@@ -1,13 +1,13 @@
 defmodule Tournament do
   defmodule Team do
-    defstruct matches: 0, won: 0, drawn: 0, lost: 0, points: 0
+    defstruct name: nil, matches: 0, won: 0, drawn: 0, lost: 0, points: 0
 
     def add_win(t), do: %Team{t | matches: t.matches + 1, won: t.won + 1, points: t.points + 3}
     def add_draw(t), do: %Team{t | matches: t.matches + 1, drawn: t.drawn + 1, points: t.points + 1}
     def add_loss(t), do: %Team{t | matches: t.matches + 1, lost: t.lost + 1}
 
-    def pretty_print(t, name) do
-      "#{String.pad_trailing(name, 31)}|  #{t.matches} |  #{t.won} |  #{t.drawn} |  #{t.lost} |  #{t.points}"
+    def pretty_print(t) do
+      "#{String.pad_trailing(t.name, 31)}|  #{t.matches} |  #{t.won} |  #{t.drawn} |  #{t.lost} |  #{t.points}"
     end
   end
 
@@ -16,7 +16,7 @@ defmodule Tournament do
     input
     |> split
     |> aggregate
-    |> convert
+    |> Map.to_list
     |> sort
     |> pretty_print
   end
@@ -31,19 +31,14 @@ defmodule Tournament do
   defp aggregate(rows) do
     rows
     |> Enum.reduce(%{}, fn {type, a, b}, acc ->
-      first = Map.get(acc, a, %Team{})
-      second = Map.get(acc, b, %Team{})
+      first = Map.get(acc, a, %Team{name: a})
+      second = Map.get(acc, b, %Team{name: b})
       case type do
         "win" -> Map.merge(acc, %{a => Team.add_win(first), b => Team.add_loss(second)})
         "draw" -> Map.merge(acc, %{a => Team.add_draw(first), b => Team.add_draw(second)})
         "loss" -> Map.merge(acc, %{a => Team.add_loss(first), b => Team.add_win(second)})
       end
     end)
-  end
-
-  defp convert(team_map) do
-    team_map
-    |> Enum.map(fn {name, team} -> {name, team} end)
   end
 
   defp sort(teams) do
@@ -53,7 +48,7 @@ defmodule Tournament do
 
   defp pretty_print(teams) do
     header = "Team                           | MP |  W |  D |  L |  P"
-    [header | Enum.map(teams, fn {name, team} -> Team.pretty_print(team, name) end)]
+    [header | Enum.map(teams, fn {_, team} -> Team.pretty_print(team) end)]
     |> Enum.join("\n")
   end
 

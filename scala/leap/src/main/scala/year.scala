a67ba115ddb68a7c66cb0a42e7261a98983aa5ca
Andrew <ahuth@mavenlink.com>
2017-04-22 23:21:22 -0700

    Scala: leap update

--- scala/leap/src/main/scala/year.scala
+++ scala/leap/src/main/scala/year.scala
@@ -1,9 +1,8 @@
 object Year {
-  def apply(year: Int): Year = {
-    new Year(year)
-  }
+  def apply(year: Int): Year = new Year(year)
 }
 
 class Year(year: Int) {
-  val isLeap = year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
+  val isLeap = mod(400) || (mod(4) && !mod(100))
+  private def mod(value: Int): Boolean = year % value == 0
 }

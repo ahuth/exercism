d410d331942b1acf32463ace8eb3de895089f5f4
Andrew Huth <andrew@huth.me>
2018-03-04 07:02:23 -0800

    Elixir: binary-search-tree

---
+++ elixir/binary-search-tree/binary_search_tree.exs
@@ -0,0 +1,41 @@
+defmodule BinarySearchTree do
+  @type bst_node :: %{data: any, left: bst_node | nil, right: bst_node | nil}
+
+  @doc """
+  Create a new Binary Search Tree with root's value as the given 'data'
+  """
+  @spec new(any) :: bst_node
+  def new(data, left \\ nil, right \\ nil) do
+    %{data: data, left: left, right: right}
+  end
+
+  @doc """
+  Creates and inserts a node with its value as 'data' into the tree.
+  """
+  @spec insert(bst_node, any) :: bst_node
+  def insert(%{data: val} = root, data) when data <= val, do: insert_left(root, data)
+  def insert(%{data: val} = root, data) when data > val, do: insert_right(root, data)
+
+  defp insert_left(%{data: val, left: nil, right: right}, data), do: new(val, new(data), right)
+  defp insert_left(node, data), do: new(node.data, insert(node.left, data), node.right)
+
+  defp insert_right(%{data: val, left: left, right: nil}, data), do: new(val, left, new(data))
+  defp insert_right(node, data), do: new(node.data, node.left, insert(node.right, data))
+
+  @doc """
+  Traverses the Binary Search Tree in order and returns a list of each node's data.
+  """
+  @spec in_order(bst_node) :: [any]
+  def in_order(tree) do
+    tree
+    |> in_order([])
+    |> Enum.reverse
+  end
+
+  defp in_order(nil, list), do: list
+  defp in_order(tree, list) do
+    left = in_order(tree.left, list)
+    middle = [tree.data | left]
+    in_order(tree.right, middle)
+  end
+end

304d892cb42f6be4804e719dfcd0e74b21933098
Andrew Huth <andrew@huth.me>
2018-05-16 20:31:15 -0700

    Ocaml: hamming (update)

--- ocaml/hamming/hamming.ml
+++ ocaml/hamming/hamming.ml
@@ -4,8 +4,7 @@ let hamming_distance first second =
   let rec calculate first second distance =
     match first, second with
       | [], [] -> Some distance
-      | a::a_tail, b::b_tail when a == b -> calculate a_tail b_tail distance
-      | _::a_tail, _::b_tail -> calculate a_tail b_tail (distance + 1)
+      | a::a_tail, b::b_tail -> calculate a_tail b_tail (distance + (if a = b then 0 else 1))
       | _ -> None
     in
       calculate first second 0

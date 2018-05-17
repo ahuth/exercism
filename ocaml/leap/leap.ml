6f12aaaef37ea2e1715304e6d34a13a23136d2ef
Andrew Huth <andrew@huth.me>
2018-05-16 18:12:32 -0700

    Ocaml: leap

---
+++ ocaml/leap/leap.ml
@@ -0,0 +1,6 @@
+let leap_year year =
+  let is_fourth = year mod 4 == 0 in
+  let is_century = year mod 100 == 0 in
+  let is_special = year mod 400 == 0 in
+  is_fourth && not is_century || is_special
+;;

43f0165dddb946a3ecc2a599a38a265bb56488e3
Andrew Huth <andrew@huth.me>
2018-05-16 21:17:02 -0700

    Ocaml: hamming (update)

--- ocaml/hamming/hamming.ml
+++ ocaml/hamming/hamming.ml
@@ -1,10 +1,9 @@
 type nucleotide = A | C | G | T
 
 let hamming_distance first second =
-  let rec calculate first second distance =
-    match first, second with
-      | [], [] -> Some distance
-      | a::a_tail, b::b_tail -> calculate a_tail b_tail (distance + (if a = b then 0 else 1))
-      | _ -> None
-    in
-      calculate first second 0
+  let rec calculate first second distance = match first, second with
+    | [], [] -> Some distance
+    | a::a_tail, b::b_tail -> calculate a_tail b_tail (distance + (if a = b then 0 else 1))
+    | _ -> None
+  in
+    calculate first second 0

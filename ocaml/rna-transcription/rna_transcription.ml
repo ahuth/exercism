c493ece18f55aa105ca8584c912e9bf3c48ec9db
Andrew Huth <andrew@huth.me>
2018-05-17 22:16:44 -0700

    Ocaml: rna_transcription (update)

--- ocaml/rna-transcription/rna_transcription.ml
+++ ocaml/rna-transcription/rna_transcription.ml
@@ -1,10 +1,8 @@
 type dna = [ `A | `C | `G | `T ]
 type rna = [ `A | `C | `G | `U ]
 
-let convert nucleotide = match nucleotide with
+let to_rna = List.map (function
   | `G -> `C
   | `C -> `G
   | `T -> `A
-  | `A -> `U
-
-let to_rna dna = List.map (convert) dna
+  | `A -> `U)

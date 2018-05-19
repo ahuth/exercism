0985b60f8086df10c34dd7d9e2ae063020a2b375
Andrew Huth <andrew@huth.me>
2018-05-18 17:15:38 -0700

    Ocaml: difference-of-squares (update)

--- ocaml/difference-of-squares/difference_of_squares.ml
+++ ocaml/difference-of-squares/difference_of_squares.ml
@@ -1,14 +1,19 @@
 open Core
 
 let square_of_sum num =
-  let list = List.range 1 (num + 1) in
-  let sum = List.fold_left ~f:(+) ~init:0 list in
+  let rec calculate num sum = match num with
+    | 0 -> sum
+    | n -> calculate (num - 1) (sum + num)
+  in
+  let sum = calculate num 0 in
   sum * sum
 
 let sum_of_squares num =
-  let list = List.range 1 (num + 1) in
-  let squares = List.map ~f:(fun x -> x * x) list in
-  List.fold_left ~f:(+) ~init:0 squares
+  let rec calculate num sum = match num with
+    | 0 -> sum
+    | n -> calculate (num - 1) (sum + num * num)
+  in
+    calculate num 0
 
 let difference_of_squares num =
   square_of_sum (num) - sum_of_squares (num)

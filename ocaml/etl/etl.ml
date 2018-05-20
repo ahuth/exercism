448ee1a12569d4ef11ac224553b775380aa7c226
Andrew Huth <andrew@huth.me>
2018-05-19 19:03:14 -0700

    Ocaml: etl (update)

--- ocaml/etl/etl.ml
+++ ocaml/etl/etl.ml
@@ -3,7 +3,5 @@ open Base
 let transform list = list
   |> List.concat_map ~f:(fun (score, letters) ->
     List.map letters ~f:(fun char -> (Char.lowercase char, score)))
-  |> List.sort ~compare:(fun a b ->
-    let (a', _) = a in
-    let (b', _) = b in
-    Char.compare a' b')
+  |> List.sort ~compare:(fun (a, _) (b, _) ->
+    Char.compare a b)

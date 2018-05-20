989e0988de29e1f6763344a85d9b4e85d55d70c4
Andrew Huth <andrew@huth.me>
2018-05-19 23:41:06 -0700

    Elixir: pangram (update)

--- elixir/pangram/pangram.exs
+++ elixir/pangram/pangram.exs
@@ -1,6 +1,5 @@
 defmodule Pangram do
   @letters ~w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
-  @alphabet Enum.join(@letters)
 
   @spec pangram?(String.t) :: boolean
   def pangram?(sentence) do
@@ -10,6 +9,7 @@ defmodule Pangram do
     |> Enum.filter(&(&1 in @letters))
     |> Enum.uniq
     |> Enum.sort
-    |> Enum.join == @alphabet
+    |> Enum.join
+    |> String.length == 26
   end
 end

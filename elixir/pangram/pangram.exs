668a11dd1992c1fd6cfd4b4fcc1532e0e0eeae90
Andrew Huth <andrew@huth.me>
2018-05-19 23:36:26 -0700

    Elixir: pangram (update)

--- elixir/pangram/pangram.exs
+++ elixir/pangram/pangram.exs
@@ -1,22 +1,15 @@
 defmodule Pangram do
-  @letters String.codepoints("abcdefghijklmnopqrstuvwxyz")
-
-  @doc """
-  Determines if a word or sentence is a pangram.
-  A pangram is a sentence using every letter of the alphabet at least once.
-
-  Returns a boolean.
-
-    ## Examples
-
-      iex> Pangram.pangram?("the quick brown fox jumps over the lazy dog")
-      true
-
-  """
+  @letters ~w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+  @alphabet Enum.join(@letters)
 
   @spec pangram?(String.t) :: boolean
   def pangram?(sentence) do
-    normalized = String.downcase(sentence)
-    Enum.all?(@letters, fn(letter) -> String.contains?(normalized, letter) end)
+    sentence
+    |> String.downcase
+    |> String.codepoints
+    |> Enum.filter(&(&1 in @letters))
+    |> Enum.uniq
+    |> Enum.sort
+    |> Enum.join == @alphabet
   end
 end

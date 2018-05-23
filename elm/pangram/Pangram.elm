191c7b0cae3109969e51680b3b3870a26c9649f2
Andrew Huth <andrew@huth.me>
2018-05-22 21:35:38 -0700

    Elm: pangram (update)

--- elm/pangram/Pangram.elm
+++ elm/pangram/Pangram.elm
@@ -5,15 +5,14 @@ isPangram : String -> Bool
 isPangram sentence =
   let length = sentence
     |> String.toLower
+    |> String.filter isLetter
     |> String.split ""
-    |> List.filter isLetter
     |> Set.fromList
-    |> Set.toList
-    |> List.length
+    |> Set.size
   in length == 26
 
-isLetter : String -> Bool
+isLetter : Char -> Bool
 isLetter char =
   List.member char alphabet
 
-alphabet = ["a" ,"b" ,"c" ,"d" ,"e" ,"f" ,"g" ,"h" ,"i" ,"j" ,"k" ,"l" ,"m" ,"n" ,"o" ,"p" ,"q" ,"r" ,"s" ,"t" ,"u" ,"v" ,"w" ,"x" ,"y" ,"z"]
+alphabet = ['a' ,'b' ,'c' ,'d' ,'e' ,'f' ,'g' ,'h' ,'i' ,'j' ,'k' ,'l' ,'m' ,'n' ,'o' ,'p' ,'q' ,'r' ,'s' ,'t' ,'u' ,'v' ,'w' ,'x' ,'y' ,'z']

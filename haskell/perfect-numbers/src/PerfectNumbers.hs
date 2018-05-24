1e5c41508996d7895ade1bcf766fa1ec72456ef7
Andrew Huth <andrew@huth.me>
2018-05-24 06:33:58 -0700

    Haskell: perfect-numbers (update)

--- haskell/perfect-numbers/src/PerfectNumbers.hs
+++ haskell/perfect-numbers/src/PerfectNumbers.hs
@@ -14,15 +14,4 @@ classify num
     total = sum (factorsOf num);
 
 factorsOf num =
-  let
-    createList possibleFactor acc
-      | possibleFactor <= num `div` 2 =
-        if isFactor possibleFactor num then
-          createList (possibleFactor + 1) (possibleFactor:acc)
-        else
-          createList (possibleFactor + 1) acc
-      | otherwise = acc
-  in
-    createList 1 [0]
-
-isFactor factor num = num `mod` factor == 0
+  filter (\factor -> num `mod` factor == 0) [1..(num `div` 2)]

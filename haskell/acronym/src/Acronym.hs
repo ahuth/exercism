99c69ea2d83d828ce7151b9ce0a863a95374d647
Andrew Huth <andrew@huth.me>
2018-05-23 21:44:35 -0700

    Haskell: acronym

---
+++ haskell/acronym/src/Acronym.hs
@@ -0,0 +1,16 @@
+module Acronym (abbreviate) where
+
+import Data.Char (isLetter, isSpace, isUpper, toUpper)
+import Data.Maybe (mapMaybe)
+
+abbreviate :: String -> String
+abbreviate xs =
+  let
+    offset = (' ':xs)
+    pairs = zip offset xs
+    getAcronymChar (prev, current)
+      | not (isLetter prev) && (isLetter current) = Just (toUpper current)
+      | not (isUpper prev) && (isUpper current) = Just current
+      | otherwise = Nothing
+  in
+    mapMaybe getAcronymChar pairs

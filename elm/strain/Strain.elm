561d0c85fd9349e47982b623ae989c479f442a01
Andrew Huth <andrew@huth.me>
2018-05-24 20:09:21 -0700

    Elm: strain

---
+++ elm/strain/Strain.elm
@@ -0,0 +1,25 @@
+module Strain exposing (discard, keep)
+
+import List exposing (reverse)
+
+
+keep : (a -> Bool) -> List a -> List a
+keep predicate list =
+    calculate predicate list []
+
+
+discard : (a -> Bool) -> List a -> List a
+discard predicate list =
+    calculate (\x -> not (predicate x)) list []
+
+
+calculate : (a -> Bool) -> List a -> List a -> List a
+calculate predicate list acc =
+    case list of
+      [] ->
+        reverse acc
+      x :: xs ->
+        if predicate x then
+          calculate predicate xs (x :: acc)
+        else
+          calculate predicate xs acc

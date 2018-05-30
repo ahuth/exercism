7672864f3e3fd33b37afc40230264efaadddce79
Andrew Huth <andrew@huth.me>
2018-05-29 21:44:49 -0700

    Haskell: hamming (update)

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,15 +1,6 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys =
-  let
-    calculate a b count =
-      case (a, b) of
-        ([], []) ->
-          Just count
-        ((a:aTail), (b:bTail)) ->
-          calculate aTail bTail (if a == b then count else count + 1)
-        _ ->
-          Nothing
-  in
-    calculate xs ys 0
+distance xs ys
+  | length xs /= length ys = Nothing
+  | otherwise = Just (length (filter (\(x, y) -> x /= y) (zip xs ys)))

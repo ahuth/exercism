43c006f00eec20a41e4437177d2b9d25dce817f1
Andrew Huth <andrew@huth.me>
2018-06-02 17:35:46 -0700

    Elm: list-ops

---
+++ elm/list-ops/ListOps.elm
@@ -0,0 +1,94 @@
+module ListOps
+    exposing
+        ( append
+        , concat
+        , filter
+        , foldl
+        , foldr
+        , length
+        , map
+        , reverse
+        )
+
+
+length : List a -> Int
+length list =
+    let
+      calculate size collection =
+        case collection of
+          [] ->
+            size
+          (x :: xs) ->
+            calculate (size + 1) xs
+    in
+      calculate 0 list
+
+reverse : List a -> List a
+reverse list =
+    let calculate first second =
+      case second of
+        [] ->
+          first
+        (head :: tail) ->
+          calculate (head :: first) tail
+    in
+      calculate [] list
+
+foldl : (a -> b -> b) -> b -> List a -> b
+foldl f acc list =
+    let
+      calculate result collection =
+        case collection of
+          [] ->
+            result
+          (head :: tail) ->
+            calculate (f head result) tail
+    in
+      calculate acc list
+
+foldr : (a -> b -> b) -> b -> List a -> b
+foldr f acc list =
+    foldl f acc (reverse list)
+
+map : (a -> b) -> List a -> List b
+map f list =
+    let
+      calculate first second =
+        case second of
+          [] ->
+            reverse first
+          (head :: tail) ->
+            calculate ((f head) :: first) tail
+    in
+      calculate [] list
+
+filter : (a -> Bool) -> List a -> List a
+filter f list =
+    let
+      calculate first second =
+        case second of
+          [] ->
+            reverse first
+          (head :: tail) ->
+            if f head then
+              calculate (head :: first) tail
+            else
+              calculate first tail
+    in
+      calculate [] list
+
+append : List a -> List a -> List a
+append xs ys =
+    let
+      calculate first second =
+        case second of
+          [] ->
+            reverse first
+          (head :: tail) ->
+            calculate (head :: first) tail
+    in
+      calculate (reverse xs) ys
+
+concat : List (List a) -> List a
+concat list =
+    foldr append [] list

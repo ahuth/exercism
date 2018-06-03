71603b200431a7b2876a6f58045d28781ea0df3d
Andrew Huth <andrew@huth.me>
2018-06-03 16:15:03 -0700

    Scala: hamming (update)

--- scala/hamming/src/main/scala/hamming.scala
+++ scala/hamming/src/main/scala/hamming.scala
@@ -1,10 +1,8 @@
 object Hamming {
-  def compute(first: String, second: String): Option[Int] = (first, second) match {
-    case (a, b) if a == b => Some(0)
+  def compute(first: String, second: String, distance: Int = 0): Option[Int] = (first, second) match {
+    case (a, b) if a == b => Some(distance)
     case (a, b) if a.length != b.length => None
-    case _ => {
-      val score = if (first.head == second.head) 0 else 1
-      compute(first.tail, second.tail).map(x => score + x)
-    }
+    case (a, b) if a.head == b.head => compute(a.tail, second.tail, distance)
+    case _ => compute(first.tail, second.tail, distance + 1)
   }
 }

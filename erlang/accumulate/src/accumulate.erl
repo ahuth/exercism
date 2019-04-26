828cf737a4b63112ced5b3c233d58861dcd5a847
Andrew Huth <andrew.huth@airbnb.com>
2019-04-26 08:44:14 -0700

    Add some whitespace in the erlang acc solution

--- erlang/accumulate/src/accumulate.erl
+++ erlang/accumulate/src/accumulate.erl
@@ -3,5 +3,5 @@
 -export([accumulate/2]).
 
 accumulate(_, []) -> [];
-accumulate(Fn, [Head|Tail]) ->
-  [Fn(Head)|accumulate(Fn, Tail)].
+accumulate(Fn, [Head | Tail]) ->
+  [Fn(Head) | accumulate(Fn, Tail)].

1f4db27392bb9a9900ffc285148efaf16a645e22
Andrew Huth <andrew@huth.me>
2019-05-15 20:48:19 -0700

    Erlang: hamming (update)

--- erlang/hamming/src/hamming.erl
+++ erlang/hamming/src/hamming.erl
@@ -1,10 +1,11 @@
 -module(hamming).
 -export([distance/2]).
+-define(LENGTH_ERROR, {error, "left and right strands must be of equal length"}).
 
 distance(Strand1, Strand2) -> distance(Strand1, Strand2, 0).
 
 distance([], [], Count) -> Count;
-distance([], _, _) -> {error, "left and right strands must be of equal length"};
-distance(_, [], _) -> {error, "left and right strands must be of equal length"};
+distance([], _, _) -> ?LENGTH_ERROR;
+distance(_, [], _) -> ?LENGTH_ERROR;
 distance([A | Tail1], [A | Tail2], Count) -> distance(Tail1, Tail2, Count);
 distance([_ | Tail1], [_ | Tail2], Count) -> distance(Tail1, Tail2, Count + 1).

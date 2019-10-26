f7811f6e10f2d44c22c4e47a87635281d75b337d
Andrew Huth <andrew@huth.me>
2019-10-26 07:22:27 -0700

    Racket: hamming (update)

--- racket/hamming/hamming.rkt
+++ racket/hamming/hamming.rkt
@@ -3,14 +3,7 @@
 (provide hamming-distance)
 
 (define (hamming-distance a b)
-  (compute-distance
+  (count
+    (negate equal?)
     (string->list a)
-    (string->list b)
-    0))
-
-(define (compute-distance a b count)
-  (cond
-    [(and (null? a) (null? b)) count]
-    [(or (null? a) (null? b)) (error "Length mismatch")]
-    [(equal? (car a) (car b)) (compute-distance (cdr a) (cdr b) count)]
-    [else (compute-distance (cdr a) (cdr b) (+ 1 count))]))
+    (string->list b)))

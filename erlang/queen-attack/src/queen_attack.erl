37669774efb7de284f84d31c4487504750c677b5
Andrew Huth <andrew.huth@airbnb.com>
2019-10-27 21:04:58 -0700

    Erlang: queen-attack (update)

--- erlang/queen-attack/src/queen_attack.erl
+++ erlang/queen-attack/src/queen_attack.erl
@@ -1,16 +1,10 @@
 -module(queen_attack).
 -export([can_attack/2]).
 
+can_attack({_, Col}, {_, Col}) -> true;
+can_attack({Row, _}, {Row, _}) -> true;
 can_attack(WhiteQueen, BlackQueen) ->
-  same_column(WhiteQueen, BlackQueen)
-    or same_row(WhiteQueen, BlackQueen)
-    or same_diagonal(WhiteQueen, BlackQueen).
-
-same_column({_, ColA}, {_, ColB}) ->
-  ColA =:= ColB.
-
-same_row({RowA, _}, {RowB, _}) ->
-  RowA =:= RowB.
+  same_diagonal(WhiteQueen, BlackQueen).
 
 same_diagonal({RowA, ColA}, {RowB, ColB}) ->
   abs(RowA - RowB) =:= abs(ColA - ColB).
